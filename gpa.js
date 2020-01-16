55fa2942024d1a41ec6d4554379304da4f49d9c4
Vic Chimenti <vicchimenti@users.noreply.github.com>
2020-01-16 10:04:20 -0800

    v1 addRow jquery script

--- gpa.js
+++ gpa.js
@@ -141,15 +141,11 @@
 		++numRows;
 		$('#unweightedTable > tbody:last-child').append(`'<tr>
 			<th>'"Course " + numRows + '</th>
-			<td>...</td>
-			<td>...</td>
+			<td><input type="text" id="grade'+numRows+'" class="grade" oninput="calculateGPA()" />
+			<label for="grade'+numRows+'" class="sr-only">'"Course "+numRows+" Letter Grade'</label></td>
+			<td><input type="text" id="credits'+numRows+'" class="credits" oninput="calculateGPA()" />
+			<label for="credits'+numRows+'" class="sr-only">'"Course "+numRows+" Courses'</label></td>
 			</tr>'`);
-	  var table = document.getElementById("unweightedTable");
-	  var row = table.insertRow(-1);
-	  var cell1 = row.insertCell(0);
-	  var cell2 = row.insertCell(1);
-	  cell1.innerHTML = "NEW CELL1";
-	  cell2.innerHTML = "NEW CELL2";
 	}
 
 
